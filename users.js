c950708f3051654cea93915bd768d752acf30fc9
Bar Wachtel <bwachtel@gmail.com>
2014-12-08 21:18:31 +0200

    Changed server from regular HTTP express app to API server

---
+++ users.js
@@ -0,0 +1,3 @@
+[{'username':'bar', 'password':'wachtel', 'screenname':'bazza'},
+ {'username':'tom', 'password':'alkalak', 'screenname':'booyaka'},
+ {'username':'tomer', 'password':'weinshtok', 'screenname':'shtok'}]
